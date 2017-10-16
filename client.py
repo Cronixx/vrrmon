d496c7dead34aae706229de71e217fa27e78468c
Jeremy Gentzer <Cronixx@users.noreply.github.com>
2017-10-16 20:01:13 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,7 @@
 #
-#   Hello World client in Python
+#   VRRMon client in Python
 #   Connects REQ socket to tcp://localhost:5555
-#   Sends "Hello" to server, expects "World" back
-#
+#   Sends <city> <station> to server, prints returned result
 
 import zmq
 import sys
